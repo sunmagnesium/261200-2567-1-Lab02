5a75faac3b9f0631679dd8c95920dbbd10cdf92a
sunmagnesium <sunzaven@gmail.com>
2024-07-04 23:33:40 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class{
+    public static void main(string[] args)
+    Car myCar =new Car(20);
+    myCar.printspeed();
+
+    Student student=new Student(fname:"sunny",lname:"mama",id:660615034);
+    student.enroll(enrolledCourse:35);
+    student.checkEnrolledCourse();
+}
